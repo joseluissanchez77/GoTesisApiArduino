d2bd66ab629dfd1f79c56fce2420b82f1a945f4a
joselsanchezb <jsanchez@redlinks.com.ec>
2022-08-05 07:49:30 -0500

    prod

--- controllers/sensorDataController.go
+++ controllers/sensorDataController.go
@@ -1,9 +1,9 @@
 package controllers
 
 import (
-	"net/http"
-	"fmt"
-	"time"
+	// "net/http"
+	// "fmt"
+	// "time"
 	// "log"
 	"github.com/joseluissanchez77/GoTesisApiArduino/models"
 	"github.com/joseluissanchez77/GoTesisApiArduino/database"
@@ -44,27 +44,25 @@ func ShowSensorData(c *gin.Context){
 
 func CreateSensorData(c *gin.Context){
 
-	
-	loc, err := time.LoadLocation("America/Guayaquil")
+	db := database.GetDatabase()
+	// // loc, erro := time.LoadLocation("America/Guayaquil")
 
-    if err != nil {
-        fmt.Println(err)
-    }
- 
-	c.JSON(http.StatusOK, time.Now().UTC().In(loc).Format("Mon 01-02-2006 15:04:05"))
-	// // dt :=  time.Now().UTC()
-
-	// // local := dt
-    // // location, err := time.LoadLocation("Europe/Budapest")
-    // // if err == nil {
-    // //     local = local.In(location)
+    // // if erro != nil {
+    // //     fmt.Println(erro)
     // // }
 
+	// // var parameterData models.ParameterData
+	// // newid := 1
+	// // // db := database.GetDatabase()
+	// // erro = db./* Select([]string{"hour_initial", "hour_end"}). */First(&parameterData, newid).Error 
 
-    // // // Format MM-DD-YYYY
-    // // // fmt.Println(dt.Format("01-02-2006"))
-	// // c.JSON(http.StatusOK,  dt.Format("15:04"), local.Location(), local.Format("15:04"))
-	// // // dt.Format("01-02-2006 15:04:05"))
+	// // if erro != nil{
+	// // 	c.JSON(400, gin.H{
+	// // 		"error" : "No se encontro datos del sensor: "+erro.Error(),
+	// // 	})
+	// // 	return
+	// // }
+	// // c.JSON(http.StatusOK, time.Now().UTC().In(loc).Format("Mon 01-02-2006 15:04:05"))
 
 	/* buf := make([]byte, 1024)
 	num, _ := c.Request.Body.Read(buf)
@@ -73,29 +71,29 @@ func CreateSensorData(c *gin.Context){
 
 	// db := database.GetDatabase()
 
-	// var sensordata models.SensorData
-	// // log.Println("---->>>", &sensordata)
-	// err := c.ShouldBindJSON(&sensordata)
+	var sensordata models.SensorData
+	// log.Println("---->>>", &sensordata)
+	err := c.ShouldBindJSON(&sensordata)
 	
-	// if err != nil{
-	// 	c.JSON(400, gin.H{
-	// 		"error" : "No se puede enlazar json: "+err.Error(),
-	// 	})
-	// 	return
-	// }
+	if err != nil{
+		c.JSON(400, gin.H{
+			"error" : "No se puede enlazar json: "+err.Error(),
+		})
+		return
+	}
 
-	// //crear
-	// err = db.Create(&sensordata).Error 
+	//crear
+	err = db.Create(&sensordata).Error 
 
-	// if err != nil{
-	// 	c.JSON(400, gin.H{
-	// 		"error" : "Error al guardar datos del sensor: "+err.Error(),
-	// 	})
-	// 	return
-	// }
+	if err != nil{
+		c.JSON(400, gin.H{
+			"error" : "Error al guardar datos del sensor: "+err.Error(),
+		})
+		return
+	}
 
 
-	// c.JSON(200, sensordata)
+	c.JSON(200, sensordata)
 }
 
 
--- models/parameterData.go
+++ models/parameterData.go
@@ -8,10 +8,12 @@ import (
 type ParameterData struct{
 	ID          uint            `json:"id" gorm:"primaryKey"`
 	Sensor string 				`json:"sensor"`
-	WeekdayEs string 			`json:"weekdayEs"`
-	WeekdayEn string 			`json:"weekdayEn"`
-	Weekday string 				`gorm:"type:enum('Sun', 'Mon', 'Tue','Wed','Thu','Fri','Sat')" json:"weekday"`
-	Hour string 				`json:"hour"`
+	WeekdayEs string 			`json:"weekday_es"`
+	WeekdayEn string 			`json:"weekday_en"`
+	// Weekday string 				`gorm:"type:enum('Sun', 'Mon', 'Tue','Wed','Thu','Fri','Sat')" json:"weekday"`
+	Weekday string 				`json:"weekday"`
+	HourInitial string 			`json:"hour_initial"`
+	HourEnd string 				`json:"hour_end"`
 	CreatedAt time.Time 		`json:"created"`
 	UpdatedAt time.Time 		`json:"updated"`
 	DeletedAt gorm.DeletedAt	`gorm:"index" json:"deleted"`

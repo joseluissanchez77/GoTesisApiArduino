cd8e73c29ea2552da278d9e608f0d6c569dfaf5d
joselsanchezb <jsanchez@redlinks.com.ec>
2022-08-05 10:39:10 -0500

    actualizar struct

--- models/sensorData.go
+++ models/sensorData.go
@@ -12,7 +12,7 @@ type SensorData struct{
 	Fahrenheit float32 			`json:"fahrenheit"`
 	WaterLevel float32 			`json:"water_level"`
 	Ph float32 					`json:"ph"`
-	Nutrition bool 				`gorm:"default:false" json:"nutrition"`
+	Nutrition int64 			`gorm:"type:integer;default:1" json:"nutrition"`
 	CreatedAt time.Time 		`json:"created"`
 	UpdatedAt time.Time 		`json:"updated"`
 	DeletedAt gorm.DeletedAt	`gorm:"index" json:"deleted"`

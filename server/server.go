8eddf223966b23cf1027e89c456bde986e4d5231
joselsanchezb <jsanchez@redlinks.com.ec>
2022-08-30 21:04:05 -0500

    -

--- server/server.go
+++ server/server.go
@@ -2,12 +2,12 @@ package server
 
 import (
 	"log"
-	"time"
+	// "time"
 	"github.com/joseluissanchez77/GoTesisApiArduino/routes"
 	"github.com/gin-gonic/gin"
 	"os"
 	// "github.com/itsjamie/gin-cors"
-	"github.com/gin-contrib/cors"
+	// "github.com/gin-contrib/cors"
 	
 )
 
@@ -29,18 +29,18 @@ func (s *Server)Run(){
 	router := routes.ConfigRoutes(s.server)
 
 	router = gin.New()  
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"PUT", "PATCH"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-		 return origin == "https://github.com"
-		},
-		MaxAge: 12 * time.Hour,
-	   }))
-	// router.Use(CORSMiddleware())
+	// // router.Use(cors.New(cors.Config{
+	// // 	AllowOrigins:     []string{"*"},
+	// // 	AllowMethods:     []string{"PUT", "PATCH"},
+	// // 	AllowHeaders:     []string{"Origin"},
+	// // 	ExposeHeaders:    []string{"Content-Length"},
+	// // 	AllowCredentials: true,
+	// // 	AllowOriginFunc: func(origin string) bool {
+	// // 	 return origin == "https://github.com"
+	// // 	},
+	// // 	MaxAge: 12 * time.Hour,
+	// //    }))
+	router.Use(CORSMiddleware())
 
 	log.Print("server is running at port: ", s.port)
 	log.Fatal(router.Run(":"+s.port))
@@ -52,14 +52,15 @@ func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
 
         c.Header("Access-Control-Allow-Origin", "*")
-        c.Header("Access-Control-Allow-Credentials", "true")
-        c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        // c.Header("Access-Control-Allow-Credentials", "true")
+        c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        // c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
         c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
 
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
-            return
-        }
+        // if c.Request.Method == "OPTIONS" {
+        //     c.AbortWithStatus(204)
+        //     return
+        // }
 
         c.Next()
     }

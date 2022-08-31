7d2f82b37147a39d464db66aeddd8de454f6a19d
joselsanchezb <jsanchez@redlinks.com.ec>
2022-08-31 06:18:28 -0500

    -

--- server/server.go
+++ server/server.go
@@ -40,7 +40,7 @@ func (s *Server)Run(){
 	// 	},
 	// 	MaxAge: 12 * time.Hour,
 	//    }))
-	// router.Use(CORSMiddleware())
+	router.Use(CORSMiddleware())
 
 	log.Print("server is running at port: ", s.port)
 	log.Fatal(router.Run(":"+s.port))
@@ -53,8 +53,8 @@ func CORSMiddleware() gin.HandlerFunc {
 
         c.Header("Access-Control-Allow-Origin", "*")
         c.Header("Access-Control-Allow-Credentials", "true")
-        // c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-        c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Header("Access-Control-Allow-Headers", "Content-Type, application/json")
+        // c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
         c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
 
         if c.Request.Method == "OPTIONS" {

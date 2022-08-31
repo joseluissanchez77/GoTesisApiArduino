fa78eb864b30ae3a18e52c0c178e95d56f6b306c
joselsanchezb <jsanchez@redlinks.com.ec>
2022-08-30 20:30:13 -0500

    -

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"time"
+	// "time"
 
-    "github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
+    // "github.com/gin-contrib/cors"
+	// "github.com/gin-gonic/gin"
 	"github.com/joseluissanchez77/GoTesisApiArduino/database"
 	"github.com/joseluissanchez77/GoTesisApiArduino/server"
 )
@@ -15,19 +15,19 @@ func main(){
 	
 	server := server.NewServer()
 
-	router := gin.Default()
+	// router := gin.Default()
 
-	router.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"*"},
-        AllowMethods:     []string{"PUT", "PATCH","GET","POST"},
-        AllowHeaders:     []string{"Origin"},
-        ExposeHeaders:    []string{"Content-Length"},
-        AllowCredentials: true,
-        AllowOriginFunc: func(origin string) bool {
-            return origin == "https://github.com"
-        },
-        MaxAge: 12 * time.Hour,
-    }))
+	// router.Use(cors.New(cors.Config{
+    //     AllowOrigins:     []string{"*"},
+    //     AllowMethods:     []string{"PUT", "PATCH","GET","POST"},
+    //     AllowHeaders:     []string{"Origin"},
+    //     ExposeHeaders:    []string{"Content-Length"},
+    //     AllowCredentials: true,
+    //     AllowOriginFunc: func(origin string) bool {
+    //         return origin == "https://github.com"
+    //     },
+    //     MaxAge: 12 * time.Hour,
+    // }))
 	server.Run()
 }
 
